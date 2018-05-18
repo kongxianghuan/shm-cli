62eee5ad6b0a20a9f54cd1826cce83dd21de2052
kongxianghuan <xianghuan.kong@gmail.com>
2018-05-18 14:53:49 +0800

    add open browser

--- src/dev.js
+++ src/dev.js
@@ -5,12 +5,15 @@ const webpackConfig = require('../webpack/webpack.dev')
 const Compiler = require('./compiler')
 const chalk = require('chalk')
 const ip = require('ip')
+const opn = require('opn')
 
 WebpackDevServer.addDevServerEntrypoints(webpackConfig, webpackConfig.devServer)
 
 const compiler = new Compiler({ config: webpackConfig })
 const devServer = new WebpackDevServer(compiler, webpackConfig.devServer)
-const tip = `Starting server on http://${ip.address()}:${webpackConfig.devServer.port}`
+const address = `http://${ip.address()}:${webpackConfig.devServer.port}`
+const tip = `Starting server on ${address}`
 devServer.listen(webpackConfig.devServer.port, webpackConfig.devServer.host, () => {
   console.log(chalk.green.bold(tip))
+  opn(address, { app: 'google chrome' })
 })
